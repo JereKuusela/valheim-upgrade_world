9e42517398a00c2b2242d4daa835075f33585c1d
Jere Kuusela <jere.kuusela@iki.fi>
2023-02-01 20:51:39 +0200

    Release 1.31

--- UpgradeWorld/UpgradeWorld.cs
+++ UpgradeWorld/UpgradeWorld.cs
@@ -8,7 +8,7 @@ public class UpgradeWorld : BaseUnityPlugin
 {
   const string GUID = "upgrade_world";
   const string NAME = "Upgrade World";
-  const string VERSION = "1.30";
+  const string VERSION = "1.31";
 #nullable disable
   public static ManualLogSource Log;
 #nullable enable
